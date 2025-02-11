2d8cbf5380f4d076e464cbdbb248d86553c10e79
LuizCarlosXavier <luizcarlosabrantes01@gmail.com>
2025-02-10 23:40:00 -0300

    fix: fix syntanx warning

--- app.py
+++ app.py
@@ -19,22 +19,21 @@ def get_completion(prompt, model="google/gemini-2.0-pro-exp-02-05:free"):
     )
     return response.choices[0].message.content
 
-prompt = f"""
-Gere uma lista com as contraindicações de um medicamento específico,\
-citando medicações que não podem ser ministradas em conjunto \
-organize em tópicos numerados. Por exemplo: \
-1. Dipirona \
-2. Rivotril \
+prompt = f"""Gere uma lista com as contraindicações de um medicamento específico,\
+citando medicações que não podem ser ministradas em conjunto,\
+organize em tópicos numerados. Por exemplo:\
+1. Dipirona\
+2. Rivotril\
 
-Se possível, forneça uma fonte confiável (sem link para páginas). Inclua também \
-situações em que o medicamento não deve ser administrado, como o caso de uma gestante \ 
-que não pode usar Roacutan. \
+Se possível, forneça uma fonte confiável (sem link para páginas). Inclua também\
+situações em que o medicamento não deve ser administrado, como o caso de uma gestante\
+que não pode usar Roacutan.\
 
-Não é necessário explicar suas decisões; retorne apenas a lista com as contraindicações \
-e a fonte, sem muitos detalhes ou observações adicionais. \
-Evite usar formatação em negrito nas palavras (por exemplo: **palavra**). \
+Não é necessário explicar suas decisões; retorne apenas a lista com as contraindicações\
+e a fonte, sem muitos detalhes ou observações adicionais.\
+Evite usar formatação em negrito nas palavras (por exemplo: **palavra**).\
 
-medicamento exemplo: amoxicilina \
+medicamento exemplo: amoxicilina\
 """
 
 response = get_completion(prompt)
