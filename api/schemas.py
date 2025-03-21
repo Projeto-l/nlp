cc69284da03ec069747ff9a83f236869bebab2ec
LuizCarlosXavier <luizcarlosabrantes01@gmail.com>
2025-03-21 09:33:53 -0300

    feat: create basemodels to input and output data

--- api/schemas.py
+++ api/schemas.py
@@ -1,2 +1,14 @@
 from pydantic import BaseModel
 from typing import List, Optional
+
+class MedicationInput(BaseModel):
+    medication1: str
+    medication2: str
+
+class MedicationInfo(BaseModel):
+    medicationName: str
+    thereIsConflict: bool
+
+class InteractionResponse(BaseModel):
+    description: str
+    alternatives: List[str]
